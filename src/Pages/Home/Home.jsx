19ecb21e817ea5168760d0bd86c42f866137dd57
Shajidaa <shajidaakterlopa101@gmail.com>
2025-10-08 00:05:12 +0600

    json and custom hooks for products

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import useProducts from '../../Hooks/Hooks';
 
 const Home = () => {
+    const {loading,error,products}=useProducts();
+    console.log({loading,error,products});
+    
     return (
         <div>
             this is home page 
