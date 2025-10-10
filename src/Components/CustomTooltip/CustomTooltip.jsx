07ccaa2363732f2ff3a08b85f5e8064c93cd2863
Shajidaa <shajidaakterlopa101@gmail.com>
2025-10-10 23:47:39 +0600

    update

--- src/Components/CustomTooltip/CustomTooltip.jsx
+++ src/Components/CustomTooltip/CustomTooltip.jsx
@@ -4,10 +4,10 @@ const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && label) {
     const data = payload[0].payload;
     return (
-      <div className="md:w-auto  w-fit bg-[#FF88] p-2.5 rounded">
-        <p className="flex flex-row justify-center items-center gap-1">
+      <div className="w-[220px]  sm-w-fit bg-[#FF88] p-2.5 rounded">
+        <p className="flex flex-row text-sm md:text-base justify-center items-center gap-1">
           {" "}
-          {data.count} Users rating this app {data.name}{" "}
+          {data.count} Users rated this app {data.name}{" "}
           <span className=" md:inline-block hidden ">
             {" "}
             <img src={rat} alt="" className="w-5 h-5 " />
